77edecc9e82d4597906ffbc580d3935907e6713d
Dan <dchen12812@gmail.com>
2015-09-27 20:48:25 -0700

    removed redundant code

--- scripts/pong.js
+++ scripts/pong.js
@@ -1,57 +1,3 @@
-//Implements a stopwatch mini game which allows the user to start/stop the stopwatch and gain a point whenever it is stopped ona full second
-
-
-// define global variables
-var cTime = 0;
-var x = 0; //successful stop
-var y = 0; //total stop
-var running = true;
-
-//Selects the canvas
-var box = document.getElementById("myCanvas");
-var ctx = box.getContext("2d");
-
-
-// define helper function format that converts time
-// in tenths of seconds into formatted Stringing A:BC.D
-function format(t){  
-    var d = t % 10;
-    var c = Math.round(((t - d) % 100) /10);
-    var b = Math.round((((t / 100) % 6) * 10) * 10)/10;
-    var a = Math.floor(t/600);
-    return(String(a) + ':' + String(b));
-}
-    
-// define event handlers for buttons; "Start", "Stop", "Reset"
-function start_handler(){   
-    if(running != true){
-        running = true;
-        timer = setInterval(function(){timer_handler()}, 100);
-    }
-}
-
-function stop_handler(){
-    if(running == true){
-        if(cTime %10 == 0){
-            x+= 1;
-        }
-        y += 1;
-        running = false;
-    }
-    clearInterval(timer);
-}
-
-function reset_handler(){
-    cTime = 0;
-    x = 0;
-    y = 0;
-}
-
-// define event handler for timer with 0.1 sec interval
-function timer_handler(){
-    cTime += 1
-}
-
 // define draw handler
 function draw_handler(canvas){
     canvas.clearRect(0, 0, box.width, box.height);
@@ -70,6 +16,10 @@ var draw = setInterval(function(){draw_handler(ctx)},10);
 
 // Implementation of classic arcade game Pong
 
+//Selects the canvas
+var box = document.getElementById("myCanvas");
+var ctx = box.getContext("2d");
+
 // initialize globals - pos and vel encode vertical info for paddles
 var WIDTH = 600;
 var HEIGHT = 400;
@@ -87,7 +37,6 @@ var ball_vel = [7,-5];
 
 // if direction is RIGHT, the ball's velocity is upper right, else upper left
 function spawn_ball(direction){
-
     ball_pos = [WIDTH/2,HEIGHT/2];
     if (direction == LEFT){
         ball_vel[0] = Math.random() * (5-2) + 2;
@@ -186,8 +135,6 @@ function draw(canvas){
 }  
         
 function keydown(key){
-
-    global paddle1_vel, paddle2_vel
     if key == simplegui.KEY_MAP["w"]:
         paddle1_vel -= 5
     if key == simplegui.KEY_MAP["up"]:
@@ -199,8 +146,6 @@ function keydown(key){
 }
         
 function keyup(key){
-
-    global paddle1_vel, paddle2_vel
     if key==simplegui.KEY_MAP["w"]:
         paddle1_vel = 0
     if key==simplegui.KEY_MAP["up"]:
@@ -212,12 +157,9 @@ function keyup(key){
 }
 
 // create frame
-frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 frame.set_keyup_handler(keyup)
-frame.add_button("Restart",new_game)
 
 // start frame
 new_game()
-frame.start()
